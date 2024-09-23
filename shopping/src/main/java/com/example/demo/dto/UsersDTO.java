3825b41e094bcdba4ecd6591a4feb487d108ec8e
aelanoh <aelanoh@localhost>
2024-09-23 14:28:13 +0900

    UserDTO 생

---
+++ shopping/src/main/java/com/example/demo/dto/UsersDTO.java
@@ -0,0 +1,25 @@
+package com.example.demo.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsersDTO {
+	
+	
+	private int userId;
+    private String username;
+    private String password;
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private boolean isSocialLogin;
+    private String socialProvider;
+}
